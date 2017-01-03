bc0315c22310d8df7034a4c42883e17f6cf5c077
Dan Warren <dan.l.warren@gmail.com>
2017-01-02 19:47:26 -0600

    Initial upload
    First crack at making functions polymorphic so they can take a list of
    minima and maxima

--- R/env.evaluate.R
+++ R/env.evaluate.R
@@ -9,49 +9,55 @@
 #' @export env.evaluate
 
 env.evaluate <- function(species, model, env, bg.source = "background", ...){
-
+  
   species <- check.bg(species, env, ...)
-
+  
   if(!inherits(species, "enmtools.species")){
     stop("Argument species must supply an enmtools.species object!")
   }
-
+  
   presence <- species$presence.points[,1:2]
   background <- species$background.points[,1:2]
-
+  
   if(inherits(model, "enmtools.model")){
     model <- model$model
   }
-
+  
   if(bg.source == "background"){
     allpoints <- rbind(presence, background)
     values <- extract(env, allpoints)
     maxes <- apply(values, 2, function(x) max(x, na.rm = TRUE))
     mins <- apply(values, 2, function(x) min(x, na.rm = TRUE))
   }
-
+  
   if(bg.source == "env") {
-    maxes <- maxValue(env)
-    mins <- minValue(env)
+    # Setting it up so we can handle either a set of rasters or a list of minima and maxima
+    if(inherits(env, c("raster", "RasterStack", "RasterBrick", "RasterLayer"))){
+      mins <- minValue(env)
+      maxes <- maxValue(env)
+    } else if (inherits(env, "list")){
+      mins <- unlist(lapply(env, min))
+      maxes <- unlist(lapply(env, max))
+    }
   }
-
-
-
+  
+  
+  
   this.lhs <- randomLHS(10000, length(names(env)))
   bg.table <- t(t(this.lhs) * (maxes  - mins) + mins)
   colnames(bg.table) <- names(env)
-
+  
   p.table <- extract(env, presence)
-#
-#   print(mins)
-#   print(maxes)
-#   print(head(bg.table))
-#   print(head(p.table))
-
+  #
+  #   print(mins)
+  #   print(maxes)
+  #   print(head(bg.table))
+  #   print(head(p.table))
+  
   pred.p <- as.numeric(predict(model, data.frame(p.table), type = "response"))
   pred.bg <- as.numeric(predict(model, data.frame(bg.table), type = "response"))
-
+  
   env.evaluation <-dismo::evaluate(pred.p, pred.bg)
-
+  
   return(env.evaluation)
 }
