d3e315b152e9c122d836e8fd5c69762b4360eaac
rdinnager <r.dinnage@gmail.com>
2017-11-16 17:06:10 +1100

    Allow calculating hypervolume on species background points.

--- R/env.species.hypervolume.R
+++ R/env.species.hypervolume.R
@@ -3,14 +3,24 @@
 #' @param species An enmtools.species object containing presence points
 #' @param env A raster or raster stack of environmental data.
 #' @param method Method to use in calculating hypervolume, See \code{\link[hypervolume]{hypervolume}} for details
+#' @param use.background Should the hypervolume be calulated on background points, instead of presence points?
 #' @param standardise Should the environmental variables be standardised before calculating the hypervolume?
 #' @param reduce_dim Should environmental variables be dimension reduced. Currently only option is "auto", which does dimension reduction using principle components analysis, only if the number of environmental variables exceeds a threshold of \code{log(# of observations)}.
 #' @param ... Additional parameters to pass on to the \code{\link[hypervolume]{hypervolume}} function
 #' @export env.species.hypervolume
-env.species.hypervolume <- function(species, env, method = "gaussian", standardise = TRUE, reduce_dim = "auto", ...) {
-  species <- add.env(species, env)
+env.species.hypervolume <- function(species, env, method = "gaussian", use.background = FALSE, standardise = TRUE, reduce_dim = "auto", ...) {
 
-  pnts <- species$presence.points[ , -c(1, 2)]
+  check.species(species)
+  if(use.background) {
+    if(is.null(species$background.points)) {
+      stop("Error: use.background is TRUE but input enmtools.species object contains no background points.")
+    }
+    species <- add.env(species, env)
+    pnts <- species$background.points[ , -c(1, 2)]
+  } else {
+    species <- add.env(species, env)
+    pnts <- species$presence.points[ , -c(1, 2)]
+  }
   hyp_dim <- ncol(pnts)
   log_obs <- log(nrow(pnts))
 
@@ -20,7 +30,7 @@ env.species.hypervolume <- function(species, env, method = "gaussian", standardi
 
   if(reduce_dim == "auto") {
     if(log_obs < hyp_dim) {
-      message("Warning: Too few prsence points for number of environmental variables. Performing principle components dimension reduction...")
+      message("Warning: Too few points for number of environmental variables. Performing principle components dimension reduction...")
       pcs <- prcomp(pnts)
       pnts <- pcs$x[ , c(1:floor(log_obs))]
     }
