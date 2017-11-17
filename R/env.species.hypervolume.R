40d71239e5caea71c7b1d51a8a6ead71465f8a73
Russell Dinnage <U1013287@UDS.anu.edu.au>
2017-11-17 12:51:37 +1100

    implemented available niche weighted sdm (enmtools.anw.sdm). Introduced a strange bug that need to be fixed.

--- R/env.species.hypervolume.R
+++ R/env.species.hypervolume.R
@@ -1,21 +1,22 @@
-#' Calculates an environmental hypervolume of presence points using Blonder et al.'s \code{\link[hypervolue]{hypervolume-package}}
+#' Calculates an environmental hypervolume of presence points using Blonder et al.'s \code{\link[hypervolume]{hypervolume-package}}
 #'
 #' @param species An enmtools.species object containing presence points
 #' @param env A raster or raster stack of environmental data.
 #' @param method Method to use in calculating hypervolume, See \code{\link[hypervolume]{hypervolume}} for details
 #' @param use.background Should the hypervolume be calulated on background points, instead of presence points?
 #' @param standardise Should the environmental variables be standardised before calculating the hypervolume?
-#' @param reduce_dim Should environmental variables be dimension reduced. Currently only option is "auto", which does dimension reduction using principle components analysis, only if the number of environmental variables exceeds a threshold of \code{log(# of observations)}.
+#' @param reduce.dim Should environmental variables be dimension reduced. Currently only option is "auto", which does dimension reduction using principle components analysis, only if the number of environmental variables exceeds a threshold of \code{log(# of observations)}.
+#' @param show.plot Should the hypervolume be plotted after calculations?
 #' @param ... Additional parameters to pass on to the \code{\link[hypervolume]{hypervolume}} function
 #' @export env.species.hypervolume
-env.species.hypervolume <- function(species, env, method = "gaussian", use.background = FALSE, standardise = TRUE, reduce_dim = "auto", ...) {
+env.species.hypervolume <- function(species, env, method = "gaussian", use.background = FALSE, standardise = TRUE, reduce.dim = "auto", show.plot = TRUE, ...) {
 
   check.species(species)
 
   species <- add.env(species, env)
 
   if(use.background) {
-    if(is.null(species$background.points)) {
+    if(class(species$background.points) != "data.frame") {
       stop("Error: use.background is TRUE but input enmtools.species object contains no background points.")
     }
     pnts <- species$background.points[ , -c(1, 2)]
@@ -29,7 +30,7 @@ env.species.hypervolume <- function(species, env, method = "gaussian", use.backg
     stop("Error: Hypervolume calculation requires at least two dimensions!")
   }
 
-  if(reduce_dim == "auto") {
+  if(reduce.dim == "auto") {
     if(log_obs < hyp_dim) {
       message("Warning: Too few points for number of environmental variables. Performing principle components dimension reduction...")
       pcs <- prcomp(pnts)
