421401cbc1cb11214eeb0cd0889ae25a114a9d8e
Dan Warren <dan.l.warren@gmail.com>
2018-01-23 16:41:38 +0100

    More import fixins

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@ globalVariables(c("..density..", "Density", "Latitude", "Longitude", "Suitabilit
 "ecospat.niche.similarity.test", "evaluate", "f", "gUnaryUnion", "gam", "gam.check",
 "geom_abline", "geom_contour", "geom_point", "geom_raster", "geom_vline", "getMRCA",
 "getValues", "getValues<-", "ggplot", "ggtitle", "grid.arrange", "guide_colourbar",
-"guides", "kable", "makereport", "maxValue", "maxent", "minValue", "predict.ppmlasso",
+"guides", "kable", "makereport", "maxValue", "maxent", "minValue", "predict", "predict.ppmlasso",
 "qplot", "randomForest", "randomLHS", "randomPoints", "rasterToPoints", "rdist", "res",
 "sampleRandom", "scale_fill_viridis", "sp.names", "stat_density_2d", "theme_bw",
 "theme_classic", "values", "xlab", "xlim", "ylim"))
