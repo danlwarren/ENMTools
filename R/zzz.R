532c6820d62c01399e49437715f302f7f0df26dc
Dan Warren <dan.l.warren@gmail.com>
2018-01-24 11:10:57 +0100

    imports

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,5 @@
 globalVariables(c("..density..", "Density", "Latitude", "Longitude", "Suitability", "X", "Y",
-"age", "bioclim", "branching.times", "circles", "coord_fixed", "descendants",
-"ecospat.grid.clim.dyn", "ecospat.niche.equivalency.test",
-"ecospat.niche.similarity.test", "evaluate", "f", "gam", "gam.check",
-"getMRCA","getValues", "getValues<-",
-"kable", "makereport", "maxValue", "maxent", "minValue", "predict",
- "randomForest", "res", "sp.names"))
+"age","f","getValues<-", "makereport", "predict", "sp.names"))
 
 #' Package setup options
 #'
