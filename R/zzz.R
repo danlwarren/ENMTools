d229f5c447aa1b6e24150502f23c9a27bd037bf4
Dan Warren <dan.l.warren@gmail.com>
2018-01-24 10:46:40 +0100

    imports

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,10 @@
-globalVariables(c("..density..", "Density", "Latitude", "Longitude", "Suitability", "X", "Y", "aes", "aes_string",
-"age", "bioclim", "branching.times", "cellStats", "circles", "coord_fixed", "descendants",
+globalVariables(c("..density..", "Density", "Latitude", "Longitude", "Suitability", "X", "Y",
+"age", "bioclim", "branching.times", "circles", "coord_fixed", "descendants",
 "ecospat.grid.clim.dyn", "ecospat.niche.equivalency.test",
-"ecospat.niche.similarity.test", "evaluate", "f", "gUnaryUnion", "gam", "gam.check",
-"geom_abline", "geom_contour", "geom_point", "geom_raster", "geom_vline", "getMRCA",
-"getValues", "getValues<-", "ggplot", "ggtitle", "grid.arrange", "guide_colourbar",
-"guides", "kable", "makereport", "maxValue", "maxent", "minValue", "predict", "predict.ppmlasso",
-"qplot", "randomForest", "randomLHS", "randomPoints", "rasterToPoints", "rdist", "res",
-"sampleRandom", "scale_fill_viridis", "sp.names", "stat_density_2d", "theme_bw",
-"theme_classic", "values", "xlab", "xlim", "ylim"))
+"ecospat.niche.similarity.test", "evaluate", "f", "gam", "gam.check",
+"getMRCA","getValues", "getValues<-",
+"kable", "makereport", "maxValue", "maxent", "minValue", "predict",
+ "randomForest", "res", "sp.names"))
 
 #' Package setup options
 #'
