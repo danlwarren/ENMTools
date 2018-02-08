7ec2fc2de3ce19f70dbcdd5b20a22ec292889a8b
Russell Dinnage <r.dinnage@gmail.com>
2018-02-08 22:24:55 +1100

    Basic leaflet maps working

---
+++ R/plot_interactive.R
@@ -0,0 +1,19 @@
+plot_interactive <- function(x, ...) {
+  dat_df <- NULL
+  if(class(x$background.points)  %in% c("data.frame", "matrix")){
+    bg_df <- as.data.frame(x$background.points[,1:2])
+    bg_df$present <- 0
+    dat_df <- rbind(dat_df, bg_df)
+  }
+  if(class(x$presence.points)  %in% c("data.frame", "matrix")){
+    pr_df <- as.data.frame(x$presence.points[ , 1:2])
+    pr_df$present <- 1
+    dat_df <- rbind(dat_df, pr_df)
+  }
+
+  m <- leaflet(dat_df) %>%
+    addProviderTiles("OpenTopoMap") %>%
+    addCircleMarkers(~Longitude, ~Latitude, color = "red")
+  m
+
+}
