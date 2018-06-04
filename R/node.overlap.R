b91f0be5d5a1180d58cfc4c0a8b82d81810cd348
Dan Warren <dan.l.warren@gmail.com>
2018-06-04 08:57:54 +0200

    Added "descendants" from phyloclim
    User pointed out that there's now an issue with importing "descendants" function, as phyloclim no longer exports it.  For the time being I just copied it into the node.overlap code.

--- R/node.overlap.R
+++ R/node.overlap.R
@@ -21,6 +21,27 @@ node.overlap <- function(overlap, tree){
 }
 
 
+# This code was copied directly from phyloclim version 0.6, as it is
+# no longer exported in the current CRAN version
+descendants <- function(tree, node, internal = FALSE, string = FALSE){
+  tips <- seq(along = tree$tip.label)
+  x <- tree$edge[,2][tree$edge[,1] == node]
+  repeat{
+    xx <- x
+    x <- sort(unique(c(x, tree$edge[,2][tree$edge[,1] %in% x])))
+    if (identical(x, xx)) break
+  }
+  # return tip number if input is tip number:
+  # -----------------------------------------
+  if (length(x) == 0) x <- node
+  if (!internal)
+    x <- x[x %in% tips]
+  if (string)
+    x <- tree$tip.label[x]
+  x
+}
+
+
 # This function takes an internal node number, overlap matrix, and tree
 # and calculates the scaled overlap using the FT method for all pairs of
 # daughter nodes
