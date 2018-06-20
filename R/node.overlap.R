a53ebdb8f59ea1c604e251ad15641e6ba3e31ce7
Russell Dinnage <u1013287@anu.edu.au>
2018-06-20 13:20:45 +1000

    Added get.descendant.tips function and replaced all uses of descendants with it.

--- R/node.overlap.R
+++ R/node.overlap.R
@@ -21,24 +21,22 @@ node.overlap <- function(overlap, tree){
 }
 
 
-# This code was copied directly from phyloclim version 0.6, as it is
-# no longer exported in the current CRAN version
-descendants <- function(tree, node, internal = FALSE, string = FALSE){
-  tips <- seq(along = tree$tip.label)
-  x <- tree$edge[,2][tree$edge[,1] == node]
-  repeat{
-    xx <- x
-    x <- sort(unique(c(x, tree$edge[,2][tree$edge[,1] %in% x])))
-    if (identical(x, xx)) break
+get.descendant.tips <- function(tree, node, internal = FALSE) {
+
+  daughters <- tree$edge[ , 2][tree$edge[ , 1] == node]
+  old_node_list <- vector()
+  new_node_list <- c(old_node_list, daughters)
+
+  while(!all(new_node_list %in% old_node_list)) {
+    old_node_list <- new_node_list
+    new_node_list <- unique(c(new_node_list, tree$edge[ , 2][tree$edge[ , 1] %in% new_node_list]))
   }
-  # return tip number if input is tip number:
-  # -----------------------------------------
-  if (length(x) == 0) x <- node
-  if (!internal)
-    x <- x[x %in% tips]
-  if (string)
-    x <- tree$tip.label[x]
-  x
+  if(internal) {
+    return(sort(new_node_list))
+  } else {
+    return(sort(new_node_list[new_node_list <= length(tree$tip.label)]))
+  }
+
 }
 
 
@@ -69,8 +67,8 @@ single.node.overlap <- function(node, overlap, tree){
 # Get the scaled overlap for a single pair of daughters
 get.daughter.overlap <- function(tree, overlap, nodes){
 
-   clade1 <- descendants(tree, nodes[1])
-   clade2 <- descendants(tree, nodes[2])
+   clade1 <- get.descendant.tips(tree, nodes[1])
+   clade2 <- get.descendant.tips(tree, nodes[2])
 
    comparisons <- expand.grid(clade1, clade2)
 
@@ -88,7 +86,7 @@ get.mult <- function (tree, tips){
 
    ntips <- length(tree$tip.label)
    mrca <- getMRCA(tree, tips)
-   nds <- descendants(tree, mrca, internal = TRUE)
+   nds <- get.descendant.tips(tree, mrca, internal = TRUE)
 
    if (identical(sort(as.integer(nds)), sort(as.integer(tips)))){
 
@@ -110,7 +108,7 @@ get.mult <- function (tree, tips){
       daughters <- sapply(nds, function(x) length(which(tree$edge[,1] == x)))
 
       # Trim it down to the number of daughters per node that falls along path between tips
-      check <- function(x, tips) any(tips %in% descendants(tree, x))
+      check <- function(x, tips) any(tips %in% get.descendant.tips(tree, x))
       id <- sapply(nds, check, tips = tips)
       mult <- 1/prod(daughters[id]) * 1/(choose(length(which(tree$edge[,1] == mrca)), 2))
    }
