273f84fd69949bbc1704347af8d297a978ea73f0
Dan Warren <dan.l.warren@gmail.com>
2018-07-19 13:27:33 +0200

    Added "bg.source" to "check.bg"
    This is now set up so that people can override the default background selection.  The next step is to include this argument in all of the modeling functions.

--- R/check.bg.R
+++ R/check.bg.R
@@ -3,10 +3,11 @@
 #' @param species An enmtools.species object
 #' @param env A raster or raster stack of environmental data.
 #' @param nback Number of background points to generate, if any
+#' @param bg.source Source for drawing background points.  If "points", it just uses the background points that are already in the species object.  If "range", it uses the range raster.  If "env", it draws points at randome from the entire study area outlined by the first environmental layer.
 #'
 #' @export check.bg
 
-check.bg <- function(species, env = NA, nback = 1000){
+check.bg <- function(species, env = NA, nback = 1000, bg.source = "default"){
 
   species <- check.species(species)
 
@@ -15,39 +16,71 @@ check.bg <- function(species, env = NA, nback = 1000){
   }
 
 
-  if(!inherits(species$background.points,"data.frame")){
-    # Now we know we need to add data points
 
-    if(inherits(species$range, c("raster", "RasterLayer", "RasterStack", "RasterBrick"))){
+  if(bg.source %in% c("points", "range", "env")){
 
-      # Drawing background points from range raster
+    # Background source manually supplied
+    print(paste("Pulling background points from", bg.source))
+
+  } else {
+
+    # Attempting to automatically determine background source
+    if(inherits(species$background.points, "data.frame")){
+
+      bg.source = "points"
+      cat("\n\nDrawing background from species background points.\n\n")
+
+    } else if(inherits(species$range, c("raster", "RasterLayer", "RasterStack", "RasterBrick"))){
+
+      # Drawing points from range raster
+      bg.source = "range"
       cat("\n\nNo background points provided, drawing background from range raster.\n\n")
-      if(nback > sum(getValues(species$range) > 0, na.rm=TRUE)){
-        species$background.points <- as.data.frame(rasterToPoints(species$range)[,1:2])
-      } else {
-        species$background.points <- as.data.frame(randomPoints(species$range, nback, species$presence.points))
-      }
 
-      colnames(species$background.points) <- colnames(species$presence.points)
-      return(species)
+    } else if(inherits(env, c("raster", "RasterLayer", "RasterStack", "RasterBrick"))) {
 
-    }
-
-    if(inherits(env, c("raster", "RasterLayer", "RasterStack", "RasterBrick"))){
-
+      # Drawing from env
       cat("\nNo background points or range raster, drawing background from environmental layers.\n\n")
-      species$background.points <- as.data.frame(randomPoints(env[[1]], nback, species$presence.points))
-      colnames(species$background.points) <- colnames(species$presence.points)
-      return(species)
+      bg.source = "env"
 
     } else {
-
-      # No range or env
+      # No usable env
       stop("No background points, range raster, or environmental data of appropriate type!")
-
     }
   }
 
-  # Background points already exist
-  return(species)
+
+
+  # Now we're adding the points
+  if(bg.source == "points"){
+    # Using points from the enmtools.species object
+    if(!inherits(species$background.points, "data.frame")){
+      stop("bg.source set to points, but species background.points does not contain a data frame!")
+    }
+    return(species)
+  }
+
+  if(bg.source == "range"){
+    # Drawing background points from range raster
+    if(!inherits(species$range, c("raster", "RasterLayer", "RasterStack", "RasterBrick"))){
+      stop("bg.source set to range, but species does not have a recognizable range raster!")
+    }
+    if(nback > sum(getValues(species$range) > 0, na.rm=TRUE)){
+      species$background.points <- as.data.frame(rasterToPoints(species$range)[,1:2])
+    } else {
+      species$background.points <- as.data.frame(randomPoints(species$range, nback, species$presence.points))
+    }
+
+    colnames(species$background.points) <- colnames(species$presence.points)
+    return(species)
+  }
+
+  if(bg.source == "env"){
+    if(!inherits(env, c("raster", "RasterLayer", "RasterStack", "RasterBrick"))){
+      stop("bg.source set to env, but env layers were not recognized!")
+    }
+    species$background.points <- as.data.frame(randomPoints(env[[1]], nback, species$presence.points))
+    colnames(species$background.points) <- colnames(species$presence.points)
+    return(species)
+  }
+
 }
