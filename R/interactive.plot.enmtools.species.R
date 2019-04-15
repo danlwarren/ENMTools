b3d6466e9e8ca9a40a041e421b99ffa7b6a5bfda
Dan Warren <dan.l.warren@gmail.com>
2019-04-15 14:32:45 +0200

    Revised interactive plots for enmtools.species objects
    Like the model plots, added the ability to turn layers on and off from the map itself.

--- R/interactive.plot.enmtools.species.R
+++ R/interactive.plot.enmtools.species.R
@@ -9,68 +9,69 @@
 #' @param map.provider Name of a map provider for the underlying interactive base map.
 #' Default is "Esri.WorldPhysical", and attractive topographic map with no place labels. A
 #' preview of all map provider options can be viewed at \url{http://leaflet-extras.github.io/leaflet-providers/preview/}
-#' @param plot.bg Should background points be plotted?
 #' @param cluster.points Should points be clustered? If TRUE, points close together
 #' will be grouped into clusters that can be interactively expanded by clicking
 #' on them.
 
-interactive.plot.enmtools.species <- function(x, map.provider = "Esri.WorldPhysical", plot.bg = FALSE, cluster.points = FALSE) {
-  dat.df <- NULL
-  if(class(x$background.points)  %in% c("data.frame", "matrix")){
-    bg.df <- as.data.frame(x$background.points[,1:2])
-    bg.df$present <- 0
-    dat.df <- rbind(dat.df, bg.df)
-  }
-  if(class(x$presence.points)  %in% c("data.frame", "matrix")){
-    pr.df <- as.data.frame(x$presence.points[ , 1:2])
-    pr.df$present <- 1
-    dat.df <- rbind(dat.df, pr.df)
-  }
+interactive.plot.enmtools.species <- function(x, map.provider = "Esri.WorldPhysical", cluster.points = FALSE) {
 
-  pal <- colorFactor(c("grey10", "red"), domain = c("0", "1"))
+  m <- leaflet() %>%
+    addProviderTiles(map.provider, group = "Base map")
 
-  if(!plot.bg) {
-    dat.df <- dat.df[dat.df$present == 1, ]
-  }
-
-  m <- leaflet(dat.df) %>%
-    addProviderTiles(map.provider)
+  overlays <- "Base map"
+  labels <- NULL
+  colors <- NULL
 
   if(class(x$range) == "RasterLayer"){
     m <- m %>%
-      addRasterImage(x$range, function(y) ifelse(is.na(y), "#00000000", "#228B2273"))
+      addRasterImage(x$range, function(y) ifelse(is.na(y), "#00000000", "#00000090"),
+                     group = "Range")
+    overlays <- c(overlays, "Range")
+    labels <- c(labels, "Range raster")
+    colors <- c(colors, "black")
   }
 
-  if(cluster.points) {
+
+  if(class(x$background.points)  %in% c("data.frame", "matrix")){
+    background.points <- x$background.points
     m <- m %>%
-      addCircleMarkers(~Longitude, ~Latitude, color = ~pal(as.factor(present)),
-                       stroke = FALSE, fillOpacity = 0.5, radius = 8,
-                       clusterOptions = markerClusterOptions())
-  } else {
-    m <- m %>%
-      addCircleMarkers(~Longitude, ~Latitude, color = ~pal(as.factor(present)),
-                       stroke = FALSE, fillOpacity = 0.5, radius = 8)
+      addCircleMarkers(~Longitude, ~Latitude, color = "red",
+                       data = background.points, stroke = FALSE, fillOpacity = 0.5,
+                       radius = 8, group = "Background points")
+    overlays <- c(overlays, "Background points")
+    labels <- c(labels, "Background points")
+    colors <- c(colors, "red")
   }
 
-  if(plot.bg) {
-    m <- m %>%
-      addLegend("bottomright", colors = "#228B22", labels = "Species Range", opacity = 0.45) %>%
-      addLegend("bottomright", pal = pal, values = c("0", "1"),
-                labFormat = function(type, x) {
-                  labs = c("0" = "Background points",
-                           "1" = "Presence points")
-                  labs[x]
-                })
-  } else {
-    m <- m %>%
-      addLegend("bottomright", colors = "#228B22", labels = "Species Range", opacity = 0.45) %>%
-      addLegend("bottomright", pal = pal, values = c("1"),
-                labFormat = function(type, x) {
-                  labs = c("0" = "Background points",
-                           "1" = "Presence points")
-                  labs[x]
-                })
+  if(class(x$presence.points)  %in% c("data.frame", "matrix")){
+    presence.points <- x$presence.points
+    if(cluster.points) {
+      m <- m %>%
+        addCircleMarkers(~Longitude, ~Latitude, color = "green",
+                         stroke = FALSE, fillOpacity = 0.5, radius = 8,
+                         data = presence.points, clusterOptions = markerClusterOptions(),
+                         group = "Occurrence points")
+    } else {
+      m <- m %>%
+        addCircleMarkers(~Longitude, ~Latitude, color = "green",
+                         data = presence.points, stroke = FALSE, fillOpacity = 0.5,
+                         radius = 8, group = "Occurrence points")
+    }
+    overlays <- c(overlays, "Occurrence points")
+    labels <- c(labels, "Occurrence points")
+    colors <- c(colors, "green")
   }
+
+  m <- m %>% addLayersControl(
+    overlayGroups = overlays,
+    options = layersControlOptions(collapsed = FALSE, position = "bottomleft")
+  )
+
+  # Build the legend given what's defined
+
+  m <- m %>%
+    addLegend("bottomright", colors = colors,
+              labels = labels)
 
   m
 
