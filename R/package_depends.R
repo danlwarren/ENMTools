97c6443e3a0699ffa0b6f1f705fdcd9e72220684
Dan Warren <dan.l.warren@gmail.com>
2020-05-29 17:51:51 +0900

    More changes to cranify
    Added roxygen docs for install.extras and an export for it. moved ResourceSelection to suggests.

--- R/package_depends.R
+++ R/package_depends.R
@@ -1,3 +1,30 @@
+#' install.extras
+#'
+#' Convenience function that installs all extra packages used in ENMTools.  ENMTools uses
+#' functions from a lot of external packages, and due to CRAN best practices it doesn't install
+#' those packages by default.  The function install.extras() just calls install.packages with a
+#' list of all of the extra packages.  At present this list includes mgcv, ecospat, randomForest,
+#' hypervolume, ape, ppmlasso, leaflet, ranger, CalibratR, caret, and ResourceSelection.
+#'
+#' @examples
+#' \dontrun{
+#' install.extras()
+#' }
+
+install.extras <- function() {
+  install.packages(c("mgcv",
+                     "ecospat",
+                     "randomForest",
+                     "hypervolume",
+                     "ape",
+                     "ppmlasso",
+                     "leaflet",
+                     "ranger",
+                     "CalibratR",
+                     "caret",
+                     "ResourceSelection"))
+}
+
 # This function checks if a package is able to be loaded and triggers an error if not
 check.package <- function(package_name) {
   if (!requireNamespace(package_name, quietly = TRUE)) {
@@ -7,18 +34,5 @@ check.package <- function(package_name) {
   }
 }
 
-#' Convenience function that installs all extra packages used in ENMTools
-#'
-install.extras <- function() {
-  install.packages(c("mgcv",
-                      "ecospat",
-                      "randomForest",
-                      "hypervolume",
-                      "ape",
-                      "ppmlasso",
-                      "leaflet",
-                      "ranger",
-                      "CalibratR",
-                      "caret",
-                      "ResourceSelection"))
-}
+
+
