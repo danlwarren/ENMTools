8b4092807321ffd0c372cb2a4348caea57850544
Dan Warren <dan.l.warren@gmail.com>
2023-02-27 11:24:27 -0500

    Fixed a few complaints from devtools::check

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,7 @@
 #' get terra to produce same output as raster::rasterToPoints
 #' @param rast A SpatRaster object
+#' @param l A list object that includes SpatVector or SpatRaster objects to wrap or unwrap
+
 rasterToPoints2 <- function(rast) {
   as.matrix(terra::as.data.frame(rast, xy = TRUE))
 }
@@ -11,5 +13,5 @@ wrap_list <- function(l) {
 
 #' export
 unwrap_list <- function(l) {
-  rapply(l, terra::wrap, classes = c("SpatVector", SpatRaster))
+  rapply(l, terra::wrap, classes = c("SpatVector", "SpatRaster"))
 }
