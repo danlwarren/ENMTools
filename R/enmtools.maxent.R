6e063bb2eb37ee33f2a5f552ff30e112f2ce523d
Dan Warren <dan.l.warren@gmail.com>
2026-02-05 11:08:46 +1100

    Fixed rts analyses for Maxent models
    Reps were being passed as vect files with environmental values, not lats and longs. Fixed now.

--- R/enmtools.maxent.R
+++ R/enmtools.maxent.R
@@ -222,12 +222,12 @@ enmtools.maxent <- function(species, env, test.prop = 0, nback = 1000, env.nback
 
       # We have to do this to capture the "this is maxent version XXX message".
       if(verbose){
-        thisrep.mx <- dismo::maxent(raster::stack(env), p = rts.df[rts.df$presence == 1,1:2], a = rts.df[rts.df$presence == 0,1:2], ...)
+        thisrep.mx <- dismo::maxent(raster::stack(env), p = crds(rts.df[rts.df$presence == 1,1:2]), a = crds(rts.df[rts.df$presence == 0,1:2]), ...)
         thisrep.model.evaluation <- dismo::evaluate(rep.species$presence.points, species$background.points,
                                                    thisrep.mx, env, na.rm = TRUE)
         thisrep.env.model.evaluation <- env.evaluate(rep.species, thisrep.mx, env, n.background = env.nback)
       } else {
-        invisible(capture.output(thisrep.mx <- dismo::maxent(raster::stack(env), p = rts.df[rts.df$presence == 1,1:2], a = rts.df[rts.df$presence == 0,1:2], ...)))
+        invisible(capture.output(thisrep.mx <- dismo::maxent(raster::stack(env), p = crds(rts.df[rts.df$presence == 1,1:2]), a = crds(rts.df[rts.df$presence == 0,1:2]), ...)))
         invisible(capture.output(thisrep.model.evaluation <- dismo::evaluate(rep.species$presence.points, species$background.points,
                                                    thisrep.mx, env, na.rm = TRUE)))
         invisible(capture.output(thisrep.env.model.evaluation <- env.evaluate(rep.species, thisrep.mx, env, n.background = env.nback)))
